8f1b2b6ae6cd7ff14416b2515fe46b7bf60b8a7b
woleshosg <55604199+woleshosg@users.noreply.github.com>
2020-07-13 21:36:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = var.aws_region
+  version = "~> 2.0"
+  assume_role {
+    role_arn     = var.account_role_arn
+    session_name = "ec2"
+  }
+}
+
+module "ec2" {
+    source      = "./modules/ec2"
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    ec2_key = var.ec2_key
+    subnet_id = var.subnet_id
+    security_group_ids = var.security_group_ids
+}
