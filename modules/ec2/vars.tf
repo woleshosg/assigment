3f18c69115d6ee1427fef5c3b8bbb26f1b32b082
woleshosg <55604199+woleshosg@users.noreply.github.com>
2020-07-13 21:36:41 +0530

    Create vars.tf

---
+++ modules/ec2/vars.tf
@@ -0,0 +1,27 @@
+variable "account_role_arn" {
+
+}
+
+variable "aws_region" {
+}
+
+variable "ami_id" {
+  
+}
+
+
+variable "instance_type" {
+  
+}
+
+variable "ec2_key" {
+  
+}
+
+variable "subnet_id" {
+  
+}
+
+variable "security_group_ids" {
+  
+}
